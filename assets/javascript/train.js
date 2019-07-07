06494bab7164ce88c72509c7ff49dd15714f1b70
Stephanie Hughes <stephaniegracehughes@gmail.com>
2019-07-06 19:06:33 -0700

    updated javascript file

---
+++ assets/javascript/train.js
@@ -0,0 +1,80 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBhwxkgylwQyham2598o1wAiZkYBwlL9S0",
+    authDomain: "train-scheduler-sgh.firebaseapp.com",
+    databaseURL: "https://train-scheduler-sgh.firebaseio.com",
+    projectId: "train-scheduler-sgh",
+    storageBucket: "",
+    messagingSenderId: "263204007633",
+    appId: "1:263204007633:web:63fe6c33e2ca4b11"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebase.database();
+
+//on click submit
+$('#submit').on('click', function(event){
+    //prevents page from refreshing
+    event.preventDefault();
+
+    //pull input information 
+    var trainName = $('#trainName').val().trim();
+    var destination = $('#destination').val().trim();
+    var trainTime = $('#trainTime').val().trim();
+    var frequency = parseInt($('#frequency').val().trim()); //returns integer for min
+    var firstTimeConverted = moment(trainTime,"HH:mm").subtract(1,'years');
+    var diffTime = moment().diff(moment(firstTimeConverted),"minutes");
+    var tRemainder = diffTime % frequency;
+    //calculate minutes away
+    var minTillTrain = frequency - tRemainder;
+    //calculate next arrival
+    var nextTrainTime = moment().add(minTillTrain, "minutes");
+    var nextArrival = moment(nextTrainTime).format('hh:mm');
+
+    console.log(trainName);
+    console.log(destination);
+    console.log(trainTime);
+    console.log(firstTimeConverted);
+    console.log(diffTime);
+    console.log(tRemainder);
+    console.log("Minutes until next train: " + minTillTrain);
+    console.log('Arrival Time: ' + nextArrival);
+    console.log(frequency);
+
+    //clear out form inputs so form is ready for next input
+    $('#trainName').val('');
+    $('#destination').val('');
+    $('#trainTime').val('');
+    $('#frequency').val('');
+
+    var newTrain = {
+        trainName: trainName,
+        destination: destination,
+        frequency: frequency,
+        nextArrival: nextArrival,
+        minutesAway:minTillTrain
+    };
+//push information to database
+    database.ref().push(newTrain);
+})
+
+database.ref().on('child_added', function(childSnapshot) {
+    var trainName = childSnapshot.val().trainName;
+    var destination = childSnapshot.val().destination;
+    var frequency = childSnapshot.val().frequency;
+    var nextArrival = childSnapshot.val().nextArrival;
+    var minTillTrain = childSnapshot.val().minutesAway;
+
+    var newRow = $('<tr>').append(
+        $('<td>').text(trainName),
+        $('<td>').text(destination),
+        $('<td>').text(frequency),
+        $('<td>').text(nextArrival),
+        $('<td>').text(minTillTrain),
+    );
+
+    $('#schedule').append(newRow);
+});
+
+
+//pull information from database and display on table
